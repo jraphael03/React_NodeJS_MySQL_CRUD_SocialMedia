4bcf29ea33815a25da668ff655a86e0850b3397c
Justin Raphael <justinraphael@Justins-MacBook-Air.local>
2021-03-21 12:02:40 -0400

    Social Media CRUD App

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+function Navbar() {
+    return (
+        <div className="navbar" >
+            <a href="/">Home</a>
+            <a href="/login">Login</a>
+            <a href="/register">Register</a>
+        </div>
+    )
+}
+
+export default Navbar
